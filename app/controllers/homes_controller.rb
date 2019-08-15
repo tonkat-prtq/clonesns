702ad6a29de5d04b293a446a898d9db750729dd5
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-08-15 11:37:52 +0900

    createメソッド作成、privateにhome_paramsメソッド作成

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -5,4 +5,23 @@ class HomesController < ApplicationController
   def new
     @home = Home.new
   end
+
+  def create
+    @home = Home.new(home_params)
+    if params[:back]
+      render :new
+    else
+      if @home.save
+        redirect_to homes_path, notice: "新規メッセージを投稿しました！"
+      else
+      render :new
+      end
+    end
+  end
 end
+
+  private
+
+  def home_params
+  params.require(:home).permit(:content)
+  end
