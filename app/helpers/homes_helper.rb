a86430f92de34170b75e68b61bcfed6ba669b15f
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-08-16 11:38:05 +0900

    createアクション時も確認画面を経由させるようhelperメソッドに追記

--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -1,6 +1,6 @@
 module HomesHelper
   def choose_new_or_edit
-    if action_name == 'new' || action_name == 'confirm'
+    if action_name == 'new' || action_name == 'confirm' || action_name == 'create'
       confirm_homes_path
     elsif action_name == 'edit'
       home_path
