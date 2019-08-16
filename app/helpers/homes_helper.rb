04af5504dda141bc0340959d7e4d7a177f9365e3
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-08-16 11:34:58 +0900

    helperメソッドの修正、formへhelperメソッドを渡して確認画面表示の分岐を実装

--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -1,9 +1,9 @@
 module HomesHelper
   def choose_new_or_edit
     if action_name == 'new' || action_name == 'confirm'
-      confirm_blogs_path
+      confirm_homes_path
     elsif action_name == 'edit'
-      blog_path
+      home_path
     end
   end
 end
